e0e05bb7f0c5d23a17af674797e4f5f5d309c469
maryam-1610 <mar.razzaq1610@gmail.com>
2025-03-08 12:00:39 -0800

    Added an list and mapped over that

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import "./App.css";
 import { Counter } from "./components";
-import {useSelector} from 'react-redux';
+import { useSelector } from "react-redux";
 
 function App() {
-  const count = useSelector((state) => state?.counter?.count)
+  const count = useSelector((state) => state?.counter?.count);
+
+  const users = ["Anthony", "Bob", "Mike", "Willson", "Taylor"];
 
   return (
     <div>
       <h1>Redux made it easy</h1>
 
       <p>Count is {count}</p>
-      <Counter name='Anthony' />
-      <Counter name='Bob' />
-      <Counter name='Mike' />
-
-
+      {users?.map((user) => (
+        <Counter name={user} />
+      ))}
     </div>
   );
 }
